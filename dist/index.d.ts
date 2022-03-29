3c090928109b36900832173aa3ad2a33193ee4d6
Moishi Netzer <moishinetzer@gmail.com>
2022-03-29 17:46:49 +0100

    Remove dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare type ClassNames = string | {
-    [key: string]: boolean;
-};
-export declare const classNames: (...items: Array<ClassNames>) => string;
-export {};
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./classnames-clone.cjs.production.min.js')
-} else {
-  module.exports = require('./classnames-clone.cjs.development.js')
-}
