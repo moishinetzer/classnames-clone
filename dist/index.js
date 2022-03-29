4c0c3491ea3412b886df0649751c74a7809706ad
Moishi Netzer <moishinetzer@gmail.com>
2022-03-29 13:28:42 +0100

    Use tsdx.io to generate

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.total = void 0;
-//List of LangCodes and Languages that use Right to Left Text
-const rtlLanguageList = {
-    ar: "Arabic",
-    arc: "Aramaic",
-    dv: "Divehi",
-    fa: "Persian",
-    ha: "Hausa",
-    he: "Hebrew",
-    khw: "Khowar",
-    ks: "Kashmiri",
-    ku: "Kurdish",
-    ps: "Pashto",
-    ur: "Urdu",
-    yi: "Yiddish",
-};
-const isRTL = (langCode) => rtlLanguageList.hasOwnProperty(langCode);
-const listRTLLangCodes = () => Object.keys(rtlLanguageList);
-const listRTLLanguages = () => Object.values(rtlLanguageList);
-const listRTLLangCodesAndCountries = () => rtlLanguageList;
-exports.total = {
-    isRTL: isRTL,
-    listRTLLangCodes: listRTLLangCodes,
-    listRTLLanguages: listRTLLanguages,
-    listRTLLangCodesAndCountries: listRTLLangCodesAndCountries,
-};
-//# sourceMappingURL=index.js.map
+
+'use strict'
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./classnames-clone.cjs.production.min.js')
+} else {
+  module.exports = require('./classnames-clone.cjs.development.js')
+}
